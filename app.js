5fcabea2c814f54fba2c853699dd2f5c5db8733c
Ibrahim Aouli <ibztheman2@gmail.com>
2023-07-07 02:00:49 +1000

    moved files around to use new mockup

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
-function openLightbox() {
-    var lightbox = document.getElementById("lightbox");
-    lightbox.style.display = "block";
-  }
-  
-  function closeLightbox() {
-    var lightbox = document.getElementById("lightbox");
-    lightbox.style.display = "none";
-  }
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var dropdownBtn = document.querySelector(".dropdown-btn");
-    dropdownBtn.addEventListener("click", openLightbox);
-  
-    var lightbox = document.getElementById("lightbox");
-    lightbox.addEventListener("click", closeLightbox);
-  });
-  
-
-  window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('sticky', window.scrollY > 0);
-  });
-
-
-function getBrands(){
-  const brands = []
-  
-  //loop through brands folder
-    //for every item -> 
-    //validate...
-    //add it to the brands array 
-  
-  //loop through brands
-    //add brand to slider
+changingText = document.querySelector('#changing-text');
+if(!changingText){
+    console.log("changing text element could not be found");
 } 
+const texts = [
+    'Creative', 'Intelligent', 'Amazing', 'cinemanuel'];
+    let currentTextIndex = 0;
+    
+    function changeText() {
+        changingText.textContent = texts[currentTextIndex];
+        currentTextIndex = (currentTextIndex + 1) % texts.length;
+    }
+    
+
+
+if(changingText){ 
+    changeText();
+    changingTextInterval = setInterval(changeText, 2000);
+}
+
+
+const sections = document.querySelectorAll('section');
+
+sections.forEach(section => {
+  section.addEventListener('click', () => {
+    scrollToSection(section.id);
+  });
+});
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
