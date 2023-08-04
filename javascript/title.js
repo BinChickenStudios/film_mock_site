4fd2045da19ffcfb6ddd595b2583dae9b4ea98f9
Ibrahim Aouli <ibztheman2@gmail.com>
2023-08-04 23:15:08 +1000

    fixes according to rays comment 03/08/2023

--- javascript/title.js
+++ javascript/title.js
@@ -4,13 +4,23 @@ const traits = [
     'Craftsmanship', 
     'Mastery', 
     'Precision', 
-    "Cine Manuel"
+    'Cine Manuel'
 ];
 
+const identities = [
+    'Storytellers',
+    'visionaries',
+    'Cine Manuel'
+]
 
-function changeTitle(titleElement, texts){
-    if(!titleElement){
-        console.log("invalid title element");
+
+function changeTitle(textElement, texts, textIndex){
+    // if(typeof textIndex !== 'number'){
+    //     console.log("invalid text index");
+    //     return;
+    // }
+    if(!textElement){
+        console.log("invalid text element");
         return;
     }
     if(!texts){
@@ -25,24 +35,30 @@ function changeTitle(titleElement, texts){
         return;
     }
 
-    textIndex %= texts.length; 
+    textIndex.index %= texts.length; 
     let text = '';
-    if(textIndex < texts.length - 1){
-         text = highlightFirstLetterInWords(texts[textIndex]);
+    if(textIndex.index < texts.length - 1){
+         text = highlightFirstLetterInWords(texts[textIndex.index]);
     }else{
-         text = highlightText(texts[textIndex]);
+         text = highlightText(texts[textIndex.index]);
     }
-    changeText(titleElement, text);
-    textIndex = (textIndex + 1) % texts.length;
+    changeText(textElement, text);
+    textIndex.index = (textIndex.index + 1) % texts.length;
 }
 
 
-let changingText = document.getElementsByClassName("title-name");
-let textIndex = 0;    
+let titleText = document.getElementsByClassName("title-name");
+let titleIndex = {index: 0};    
+let aboutText = document.getElementsByClassName("about-name");
+let aboutIndex = 0;    
 
-if (!changingText) {
+if (!titleText) {
     console.log("changing text element could not be found");
 }else{
-    textIndex = 0;
-    setInterval(() => changeTitle(changingText, traits), 6000);
+    titleIndex = {index: 0};
+    aboutIndex = {index: 0};
+    changeTitle(titleText, traits, titleIndex);
+    changeTitle(aboutText, identities, aboutIndex);
+    setInterval(() => changeTitle(titleText, traits, titleIndex), 3000);
+    setInterval(() => changeTitle(aboutText, identities, aboutIndex), 3000);
 }
